0e00dc6860fc1a5096522e3522d7884d06a1c5f4
Enes <enesaygur387@gmail.com>
2026-07-18 22:19:39 +0300

    feat: create main layout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,9 +1,17 @@
 import { Outlet } from "react-router";
+import Header from "../components/layout/Header";
+import Footer from "../components/layout/Footer";
 
 function MainLayout() {
   return (
     <>
-      <Outlet />
+      <Header />
+
+      <main>
+        <Outlet />
+      </main>
+
+      <Footer />
     </>
   );
 }
