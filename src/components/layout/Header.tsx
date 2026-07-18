ba4ee74e5c029178b71a3d65351444b51548478f
Enes <enesaygur387@gmail.com>
2026-07-18 22:45:08 +0300

    style: add header styles

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,37 +1,47 @@
 import { NavLink } from "react-router";
-
+import styles from "./Header.module.css";
 function Header() {
   return (
-    <header>
-      <h2>NovaShop</h2>
-      <nav>
+    <header className={styles.header}>
+      <h2 className={styles.logo}>NovaShop</h2>
+      <nav className={styles.nav}>
         <NavLink
           to="/"
-          className={({ isActive }) => (isActive ? "active" : "")}
+          className={({ isActive }) =>
+            isActive ? `${styles.link} ${styles.active}` : styles.link
+          }
         >
           Home
         </NavLink>
         <NavLink
           to="/products"
-          className={({ isActive }) => (isActive ? "active" : "")}
+          className={({ isActive }) =>
+            isActive ? `${styles.link} ${styles.active}` : styles.link
+          }
         >
           Products
         </NavLink>
         <NavLink
           to="/wishlist"
-          className={({ isActive }) => (isActive ? "active" : "")}
+          className={({ isActive }) =>
+            isActive ? `${styles.link} ${styles.active}` : styles.link
+          }
         >
           Wishlist
         </NavLink>
         <NavLink
           to="/cart"
-          className={({ isActive }) => (isActive ? "active" : "")}
+          className={({ isActive }) =>
+            isActive ? `${styles.link} ${styles.active}` : styles.link
+          }
         >
           Cart
         </NavLink>
         <NavLink
           to="/login"
-          className={({ isActive }) => (isActive ? "active" : "")}
+          className={({ isActive }) =>
+            isActive ? `${styles.link} ${styles.active}` : styles.link
+          }
         >
           Login
         </NavLink>
