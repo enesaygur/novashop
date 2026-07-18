5f4c9b2a032ed5bb7702c2618c92ca285e843164
Enes <enesaygur387@gmail.com>
2026-07-18 22:39:49 +0300

    feat: create application header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,43 @@
+import { NavLink } from "react-router";
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <h2>NovaShop</h2>
+      <nav>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/products"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Products
+        </NavLink>
+        <NavLink
+          to="/wishlist"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Wishlist
+        </NavLink>
+        <NavLink
+          to="/cart"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Cart
+        </NavLink>
+        <NavLink
+          to="/login"
+          className={({ isActive }) => (isActive ? "active" : "")}
+        >
+          Login
+        </NavLink>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
