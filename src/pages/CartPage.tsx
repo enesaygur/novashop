492e3c6ca0cc6a531d0c7673d5bfb59aae69950d
Enes <enesaygur387@gmail.com>
2026-07-21 00:49:35 +0300

    feat: add cart page

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,5 +1,30 @@
+import { Link } from "react-router";
+import { useCart } from "../hooks/useCart";
+import styles from "./CartPage.module.css";
 function CartPage() {
-  return <div>CartPage</div>;
+  const { items } = useCart();
+  return (
+    <div className={styles.container}>
+      <h1>Shopping Cart</h1>
+      {items.length === 0 ? (
+        <p>Your cart is empty</p>
+      ) : (
+        <div className={styles.list}>
+          {items.map((item) => (
+            <div key={item.product.id} className={styles.item}>
+              <img src={item.product.thumbnail} alt={item.product.title} />
+              <div>
+                <h3>{item.product.title}</h3>
+                <p>${item.product.price}</p>
+                <p>Quantity: {item.quantity}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+      <Link to="/products">Continue Shopping</Link>
+    </div>
+  );
 }
 
 export default CartPage;
