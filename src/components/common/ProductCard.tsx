65b845a876d421b820bb1a81ec51a0e6a34a3cb5
Enes <enesaygur387@gmail.com>
2026-07-20 23:56:52 +0300

    feat: add add-to-cart button

--- src/components/common/ProductCard.tsx
+++ src/components/common/ProductCard.tsx
@@ -1,25 +1,32 @@
 import { Link } from "react-router";
 import type { Product } from "../../types/Product";
 import styles from "./ProductCard.module.css";
+import { useCart } from "../../hooks/useCart";
 type ProductCardProps = {
   product: Product;
 };
 function ProductCard({ product }: ProductCardProps) {
+  const { addItem } = useCart();
   return (
-    <Link to={`/products/${product.id}`} className={styles.card}>
-      <img
-        className={styles.image}
-        src={product.thumbnail}
-        alt={product.title}
-      />
-      <div className={styles.content}>
-        <h3>{product.title}</h3>
-        <p className={styles.brand}>{product.brand}</p>
-        <p className={styles.price}>${product.price}</p>
-        <p>⭐ {product.rating}</p>
-        <p>Stock: {product.stock}</p>
-      </div>
-    </Link>
+    <article className={styles.card}>
+      <Link to={`/products/${product.id}`} className={styles.link}>
+        <img
+          className={styles.image}
+          src={product.thumbnail}
+          alt={product.title}
+        />
+        <div className={styles.content}>
+          <h3>{product.title}</h3>
+          <p className={styles.brand}>{product.brand}</p>
+          <p className={styles.price}>${product.price}</p>
+          <p>⭐ {product.rating}</p>
+          <p>Stock: {product.stock}</p>
+        </div>
+      </Link>
+      <button type="button" onClick={() => addItem(product)}>
+        Add to cart
+      </button>
+    </article>
   );
 }
 
