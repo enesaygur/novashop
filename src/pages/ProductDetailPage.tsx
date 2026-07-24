c89ce42feeb4fa21dc82e18dbb2518bccf436405
Enes <enesaygur387@gmail.com>
2026-07-24 23:39:58 +0300

    feat: improve product details page

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -1,24 +1,41 @@
 import { useParams } from "react-router";
 import { useProduct } from "../hooks/useProduct";
+import { useCart } from "../hooks/useCart";
+import styles from "./ProductDetailPage.module.css";
 
 function ProductDetailPage() {
   const { id } = useParams();
   const productId = Number(id);
   const { data, isPending, error } = useProduct(productId);
+  const { addItem } = useCart();
 
   if (isPending) return <h1>Loading...</h1>;
   if (error) return <h2>Something went wrong</h2>;
   if (!data) return <h2>Product not found</h2>;
   return (
-    <>
-      <img src={data.thumbnail} alt={data.title} />
-      <h1>{data.title}</h1>
-      <p>{data.description}</p>
-      <h2>${data.price}</h2>
-      <p>⭐ {data.rating}</p>
-      <p>Brand: {data.brand}</p>
-      <p>Stock: {data.stock}</p>
-    </>
+    <div className={styles.container}>
+      <div className={styles.imageSection}>
+        <img src={data.thumbnail} alt={data.title} className={styles.image} />
+      </div>
+
+      <div className={styles.info}>
+        <p className={styles.brand}>{data.brand}</p>
+
+        <h1>{data.title}</h1>
+
+        <div className={styles.rating}>⭐ {data.rating}</div>
+
+        <p className={styles.description}>{data.description}</p>
+
+        <p className={styles.price}>${data.price}</p>
+
+        <p className={styles.stock}>Stock: {data.stock}</p>
+
+        <button onClick={() => addItem(data)} disabled={data.stock === 0}>
+          {data.stock === 0 ? "Out of Stock" : "Add to Cart"}
+        </button>
+      </div>
+    </div>
   );
 }
 
