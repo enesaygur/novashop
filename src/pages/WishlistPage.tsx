c1d43693543728f6ffbcd74be12e957dcc80ae95
Enes <enesaygur387@gmail.com>
2026-07-25 12:35:00 +0300

    feat: add wishlist page

--- src/pages/WishlistPage.tsx
+++ src/pages/WishlistPage.tsx
@@ -1,5 +1,36 @@
+import { Link } from "react-router";
+import EmptyState from "../components/common/EmptyState";
+import { useWishlist } from "../hooks/useWishlist";
+import styles from "./WishlistPage.module.css";
+
 function WishlistPage() {
-  return <div>WishlistPage</div>;
+  const { items, removeItem } = useWishlist();
+  if (items.length === 0)
+    return <EmptyState message="Your wishlist is empty" />;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Wishlist</h1>
+
+      <div className={styles.grid}>
+        {items.map((product) => (
+          <div key={product.id} className={styles.card}>
+            <Link to={`/product/${product.id}`} className={styles.link}>
+              <img
+                src={product.thumbnail}
+                alt={product.title}
+                className={styles.image}
+              />
+              <div className={styles.content}>
+                <h2>{product.title}</h2>
+                <p className={styles.price}>${product.price}</p>
+              </div>
+            </Link>
+            <button onClick={() => removeItem(product.id)}>Remove</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default WishlistPage;
