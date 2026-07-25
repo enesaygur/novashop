51b1348590f5b30673d2c933b6cd87883fbb4699
Enes <enesaygur387@gmail.com>
2026-07-25 23:49:51 +0300

    feat: add login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,47 @@
+import { useState } from "react";
+import styles from "./LoginPage.module.css";
+import { useNavigate } from "react-router";
+import { useAuth } from "../hooks/useAuth";
 function LoginPage() {
-  return <div>LoginPage</div>;
+  const navigate = useNavigate();
+  const { login } = useAuth();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    login({
+      id: crypto.randomUUID(),
+      name,
+      email,
+    });
+
+    navigate("/");
+  }
+  return (
+    <div className={styles.container}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <h1>Login</h1>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default LoginPage;
